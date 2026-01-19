70fb044893730b89c25397143a4f4f9a6378a062
Anton <lakirienkol@gmail.com>
2026-01-19 10:49:37 +0200

    remove download btn

--- components/Sections/Warehouse.vue
+++ components/Sections/Warehouse.vue
@@ -37,7 +37,7 @@ const { t } = useI18n()
           <p class="text-sm lg:text-lg">{{ t('warehouse.type2Desk2') }}</p>
         </div>
       </div>
-      <div class="flex justify-center lg:justify-end mt-14">
+      <!-- <div class="flex justify-center lg:justify-end mt-14">
         <Button
           :text="t('warehouse.button')"
           variant="primary"
@@ -48,7 +48,7 @@ const { t } = useI18n()
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 12.088L3.95543 8.04457L4.76457 7.22171L7.42857 9.88571V0H8.57143V9.88571L11.2343 7.22286L12.0446 8.04457L8 12.088ZM1.84686 16C1.32038 16 0.881143 15.824 0.529143 15.472C0.177143 15.12 0.000761905 14.6804 0 14.1531V11.384H1.14286V14.1531C1.14286 14.3291 1.216 14.4907 1.36229 14.6377C1.50857 14.7848 1.66971 14.8579 1.84571 14.8571H14.1543C14.3295 14.8571 14.4907 14.784 14.6377 14.6377C14.7848 14.4914 14.8579 14.3299 14.8571 14.1531V11.384H16V14.1531C16 14.6796 15.824 15.1189 15.472 15.4709C15.12 15.8229 14.6804 15.9992 14.1531 16H1.84686Z" fill="#002160"></path></svg>
           </template>
         </Button>
-      </div>
+      </div> -->
     </div>
   </section>
 </template>
